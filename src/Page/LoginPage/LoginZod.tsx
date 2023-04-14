24998dd24359c13391cad9b0d540c51a8fe51b81
rlopxhan21 <rlopxhan21@gmail.com>
2023-04-15 09:55:21 +1000

    Add email and password similirity validation

--- src/Page/LoginPage/LoginZod.tsx
+++ src/Page/LoginPage/LoginZod.tsx
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 export const schema = z.object({
   email: z.string().email({ message: "Please enter a valid email address." }),
-  password: z.string().min(6, {
-    message: "The password you provided must have at least 6 characters.",
+  password: z.string().min(8, {
+    message: "The password you provided must have at least 8 characters.",
   }),
 });
 
--- src/Page/RegisterPage/RegisterZod.tsx
+++ src/Page/RegisterPage/RegisterZod.tsx
@@ -6,16 +6,23 @@ export const schema = z
       .string()
       .min(1, { message: "Full Name must have at least a character long." }),
     email: z.string().email({ message: "Please enter a valid email address." }),
-    password: z.string().min(6, {
-      message: "The password you provided must have at least 6 characters.",
+    password: z.string().min(8, {
+      message: "The password you provided must have at least 8 characters.",
     }),
-    password2: z.string().min(6, {
-      message: "The password you provided must have at least 6 characters.",
+    password2: z.string().min(8, {
+      message: "The password you provided must have at least 8 characters.",
     }),
   })
   .refine((data) => data.password === data.password2, {
     message: "Your password and confirm password must match.",
     path: ["password2"],
-  });
+  })
+  .refine(
+    (data) => data.email.split("@")[0].includes(data.password.toLowerCase()),
+    {
+      message: "Your password is too similar to your email!",
+      path: ["password"],
+    }
+  );
 
 export type RegisterDataType = z.infer<typeof schema>;
